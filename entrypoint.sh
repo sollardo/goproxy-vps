7a41ed849e7ac572bc56e2c787cc8dc7538b79ee
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-14 22:10:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,1 @@
- cd /opt/goproxy
- cat <<EOF >goproxy-vps.toml
-[default]
-log_level = 2
-
-[[http2]]
-listen = ":443"
-server_name = ["$SERVER_NAME"]
-proxy_fallback = "http://127.0.0.1:80"
-EOF
-
-goproxy-vps -logtostderr -v 3
+ /opt/goproxy/goproxy-vps $CONFIG_FILE_URL -logtostderr -v 3
